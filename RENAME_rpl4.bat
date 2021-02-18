747ec2bc5bf25e4e53a6ce5fe095df19c3cf362a
soriddo_liQuid <dannmakuparanoia@gmail.com>
2021-02-18 15:16:04 +0900

    リプレイファイルの最初の日付を削除するバッチファイルを作成。

---
+++ RENAME_rpl4.bat
@@ -0,0 +1,23 @@
+@echo off
+REM --------------------------------------
+REM rpl4フォルダの中に入れて使用すること。
+REM 日付を消すことができる。
+REM --------------------------------------
+set processCount=0
+@echo on
+for %%F in (????_????_??????_*.rpl4_1010) do call :sub "%%F"
+@echo off
+echo Renamed %processCount% files!
+REM powershell sleep 1
+pause
+exit /b
+
+:sub
+  @echo off
+  set /a processCount=%processCount%+1
+  set BEFORE=%~1
+  set FILENAME=%~n1
+  set EXTENSION=%~x1
+  set AFTER=%FILENAME:~17%%EXTENSION%
+  @echo on
+  ren "%BEFORE%" "%AFTER%"
